f2bece835bf2f9d813ca358a6c7c0144379da250
Elijah <elijahahmad03@gmail.com>
2024-04-19 15:11:00 +0700

    Frontend: Added loading animation and styling checker.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import React, {useLayoutEffect, useRef} from "react";
-import gsap from "gsap";
-import {LoadingScreen} from "./components/loading-screen/LoadingScreen";
+import React, { useRef } from 'react';
+import { LoadingScreen } from './components/loading-screen/LoadingScreen';
 
 const App : React.FC = () => {
   const comp = useRef(null);
   const disabledAnimation = false;
 
   return (
-    <div ref={comp} >
-      {!disabledAnimation && <LoadingScreen comp={comp} disabled={disabledAnimation}/> }
+    <div ref={comp}>
+      {!disabledAnimation && <LoadingScreen comp={comp} disabled={disabledAnimation} /> }
     </div>
   );
-}
+};
 
 export default App;
-
