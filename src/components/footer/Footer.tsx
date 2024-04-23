927f42bea0e5bb766d9f5e80fe9e4ba9c7c7753e
Elijah <elijahahmad03@gmail.com>
2024-04-23 14:56:25 +0700

    Frontend: Added landing page design.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Logo } from '../logo/Logo';
 
 const Footer = () => (
-  <footer className="p-4 bg-gray-50 sm:p-6 dark:bg-black border-t border-white">
+  <footer className="p-4 bg-gray-50 sm:p-6 dark:bg-black border-t border-black dark:border-white">
     <div className="mx-auto max-w-screen-xl">
       <div className="md:flex md:justify-between">
         <div className="mb-6 md:mb-0">
@@ -16,7 +16,7 @@ const Footer = () => (
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Technical Material</h2>
               <ul className="text-gray-600 dark:text-gray-400">
                 <li className="mb-4">
-                  <Link to="/" className="hover:underline ">Introduction Documentation</Link>
+                  <Link to="https://viranova.gitbook.io/viraml" className="hover:underline ">Introduction Documentation</Link>
                 </li>
                 <li>
                   <Link to="/" className="hover:underline">Technical Roadmap</Link>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -16,11 +16,11 @@ const Navbar: React.FC = () => {
   }, [darkMode]);
 
   return (
-    <nav className="bg-white border-b border-white px-4 lg:px-6 py-2.5 h-16 dark:bg-black  sticky top-0 z-20">
+    <nav className="bg-white border-b border-black dark:border-white px-4 lg:px-6 py-2.5 h-16 dark:bg-black sticky top-0 z-20">
       <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl h-full">
         <Logo link="/" collapsed />
         <div className={`${menuOpened ? 'flex' : 'hidden'} justify-between items-center w-full lg:flex lg:w-auto lg:order-1`} id="mobile-menu-2">
-          <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 w-full">
+          <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 w-full items-center">
             <li>
               <Link
                 to="/"
@@ -54,8 +54,33 @@ const Navbar: React.FC = () => {
                 Contact
               </Link>
             </li>
+
+            {/* Toggle Dark Mode */}
+            <button
+              data-tooltip-target="toggle-colors-example-toggle-dark-mode-tooltip"
+              type="button"
+              data-toggle-dark="dark"
+              className="flex items-center w-9 h-9 justify-center text-xs font-medium text-gray-700 bg-white border border-gray-200 rounded-lg toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-900 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-400"
+              onClick={() => { setDarkMode(!darkMode); }}
+            >
+              <svg
+                data-toggle-icon="moon"
+                className={`w-3.5 h-3.5 ${!darkMode ? '' : 'hidden'}`}
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 18 20"
+              >
+                <path d="M17.8 13.75a1 1 0 0 0-.859-.5A7.488 7.488 0 0 1 10.52 2a1 1 0 0 0 0-.969A1.035 1.035 0 0 0 9.687.5h-.113a9.5 9.5 0 1 0 8.222 14.247 1 1 0 0 0 .004-.997Z" />
+              </svg>
+              <svg data-toggle-icon="sun" className={`w-3.5 h-3.5 ${darkMode ? '' : 'hidden'}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 15a5 5 0 1 0 0-10 5 5 0 0 0 0 10Zm0-11a1 1 0 0 0 1-1V1a1 1 0 0 0-2 0v2a1 1 0 0 0 1 1Zm0 12a1 1 0 0 0-1 1v2a1 1 0 1 0 2 0v-2a1 1 0 0 0-1-1ZM4.343 5.757a1 1 0 0 0 1.414-1.414L4.343 2.929a1 1 0 0 0-1.414 1.414l1.414 1.414Zm11.314 8.486a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM4 10a1 1 0 0 0-1-1H1a1 1 0 0 0 0 2h2a1 1 0 0 0 1-1Zm15-1h-2a1 1 0 1 0 0 2h2a1 1 0 0 0 0-2ZM4.343 14.243l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414a1 1 0 0 0-1.414-1.414ZM14.95 6.05a1 1 0 0 0 .707-.293l1.414-1.414a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 .707 1.707Z" />
+              </svg>
+              <span className="sr-only">Toggle dark/light mode</span>
+            </button>
           </ul>
         </div>
+
       </div>
     </nav>
   );
