22dfcd1aaaaf8048970b675eac20d4076ddba852
Elijah <aa1719@imperial.ac.uk>
2024-04-27 21:56:49 +0700

    Frontend: Added explanation.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -22,37 +22,37 @@ const Navbar: React.FC = () => {
         <div className={`${menuOpened ? 'flex' : 'hidden'} justify-between items-center w-full lg:flex lg:w-auto lg:order-1`} id="mobile-menu-2">
           <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 w-full items-center">
             <li>
-              <Link
-                to="/"
+              <p
                 className="block py-2 pr-4 pl-3 text-white rounded bg-blue-700 lg:bg-transparent lg:text-blue-700 lg:p-0 dark:text-white"
                 aria-current="page"
               >
                 Home
-              </Link>
+              </p>
             </li>
             <li>
-              <Link
-                to="/"
+              <a
+                href="#TopBanner"
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                aria-current="page"
               >
-                Company
-              </Link>
+                About
+              </a>
             </li>
             <li>
-              <Link
-                to="/"
+              <a
+                href="#BottomBanner"
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Team
-              </Link>
+                How
+              </a>
             </li>
             <li>
-              <Link
-                to="/"
+              <a
+                href="#WhyMain"
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Contact
-              </Link>
+                Why
+              </a>
             </li>
 
             {/* Toggle Dark Mode */}
